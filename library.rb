9ea370ccfb11a02e2422327f28b9dedffc6295da
kimu805 <kimukimuok805@gmail.com>
2024-12-18 21:31:06 +0900

    Update library.rb

--- library.rb
+++ library.rb
@@ -1,8 +1,31 @@
-p /Ruby/ === "I love Ruby"
+n = gets.to_i
+s = gets.chomp
 
-p case "I love Ruby"
-when /Ruby/
-  "Ruby!"
-when /Java/
-  "Java!"
+if n % 2 == 1
+  one_count = ( n + 1 ) / 2 - 1
+  one_count.times do |i|
+    if s[i] != "1"
+      puts "No"
+      exit
+    end
+  end
+
+  slash_position = ( n + 1 ) / 2 - 1
+  if s[slash_position] != "/"
+    puts "No"
+    exit
+  end
+
+  two_count = n - (( n + 1 ) / 2 + 1)
+  two_count.times do |i|
+    if s[slash_position + i + 1] != "2"
+      puts "No"
+      exit
+    end
+  end
+
+  puts "Yes"
+
+else
+  p "No"
 end
