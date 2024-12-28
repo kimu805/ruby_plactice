b0ca072a52bb22f5d41121dea09ad9f42783b091
kimu805 <kimukimuok805@gmail.com>
2024-12-28 18:19:08 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,16 +1,6 @@
-def in1to10(num, outside_mode)
-  if outside_mode
-    puts "True"
-  else
-    if num >= 1 && num <= 10
-      puts "True"
-    else
-      puts "False"
-    end
-  end
-end
+fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
 
-# 呼び出し例
-in1to10(5,false)
-in1to10(11,false) 
-in1to10(11,true) 
+fruits_price.each do |fruit_price|
+  total_price = fruit_price[1].sum
+  p "#{fruit_price[0]}の合計金額は#{total_price}円です"
+end
