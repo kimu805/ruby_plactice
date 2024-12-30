8248fefd03a554db8740ca305c2dc4760ecce224
kimu805 <kimukimuok805@gmail.com>
2024-12-30 16:09:23 +0900

    Update drill.rb

--- drill.rb
+++
@@ -1,6 +0,0 @@
-fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
-
-fruits_price.each do |fruit_price|
-  total_price = fruit_price[1].sum
-  p "#{fruit_price[0]}の合計金額は#{total_price}円です"
-end
