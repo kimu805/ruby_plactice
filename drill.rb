e4df1abebf540db157851aefff1b030d487fee22
kimu805 <kimukimuok805@gmail.com>
2025-01-02 18:21:04 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,10 +1,51 @@
-while true do
-  puts "【0】：カロリーを表示する、【1】：終了する"
-  input = gets.to_i
+def register_book
+  # 本の著者、タイトル、価格をユーザーに入力させ、保存する
+  puts '著者を入力してください'
+  author = gets.chomp
+  
+  puts 'タイトルを入力してください'
+  title = gets.chomp
 
-  if input == 0
-    puts "500kcal"
-  elsif input == 1
+  puts '価格を入力してください'
+  price = gets.to_i
+
+  book = { author: author, title: title, price: price }
+  return book
+end
+
+def show_books(books)
+  puts "見たい番号を入力してください"
+  # 保存された本の一覧を出力する
+  books.each do |book|
+    show_detail(book)
+  end
+end
+
+def show_detail(book)
+  # 選択された本の詳細な情報（著者、タイトル、価格）を出力する
+  puts "著者 #{book[:author]}"
+  puts "タイトル #{book[:title]}"
+  puts "価格 #{book[:price]}円"
+end
+
+books = []
+
+while true do
+  puts "番号を入力してください"
+  puts "0: 本を登録する"
+  puts "1: 本の一覧を見る"
+  puts "2: 終了する"
+  case gets.to_i
+  when 0
+    # 本の登録を行う
+    book = register_book
+    books << book
+  when 1
+    # 保存された本の一覧を出力する
+    show_books(books)
+  when 2
     exit
+  else
+    puts '無効な値です'
   end
 end
