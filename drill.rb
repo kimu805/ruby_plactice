691f7cd42af057ac14f516b34ec6292c669c32a2
kimu805 <kimukimuok805@gmail.com>
2025-01-02 20:24:41 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,35 +1,32 @@
-def register_book
-  # 本の著者、タイトル、価格をユーザーに入力させ、保存する
+def register_book(books)
   puts '著者を入力してください'
   author = gets.chomp
-  
   puts 'タイトルを入力してください'
   title = gets.chomp
-
   puts '価格を入力してください'
   price = gets.to_i
-
   book = { author: author, title: title, price: price }
-  return book
+  books << book
 end
 
 def show_books(books)
   puts "見たい番号を入力してください"
-  # 保存された本の一覧を出力する
+  index = 1
   books.each do |book|
-    show_detail(book)
+    puts "#{index}: #{book[:title]}"
+    index += 1
   end
+  input = gets.to_i
+  show_detail(books[input - 1])
 end
 
 def show_detail(book)
-  # 選択された本の詳細な情報（著者、タイトル、価格）を出力する
   puts "著者 #{book[:author]}"
   puts "タイトル #{book[:title]}"
   puts "価格 #{book[:price]}円"
 end
 
 books = []
-
 while true do
   puts "番号を入力してください"
   puts "0: 本を登録する"
@@ -37,11 +34,8 @@ while true do
   puts "2: 終了する"
   case gets.to_i
   when 0
-    # 本の登録を行う
-    book = register_book
-    books << book
+   register_book(books)
   when 1
-    # 保存された本の一覧を出力する
     show_books(books)
   when 2
     exit
