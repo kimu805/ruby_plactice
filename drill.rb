d3b02c152b7f0e25b462005187c4b23a00ced4f0
kimu805 <kimukimuok805@gmail.com>
2025-01-02 16:57:02 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,34 +1,16 @@
-# def lone_sum(array)
-#   unique_elements = array.select { |number| array.count(number) == 1 }
+def near_ten(number)
+  total = (number / 100) + (number / 10 % 10) + (number % 10)
+  x_ten = total.round(-1)
+  
+  diff = (total - x_ten).abs
 
-#   p unique_elements.sum
-# end
-
-
-def lone_sum(array)
-  unique_array = []
-
-  array.each do |num|
-    count = 0
-
-    array.each do |i|
-      if num == i
-        count += 1
-      end
-    end
-
-    if count < 2
-      unique_array << num
-    end
+  if diff <= 2
+    p "True"
+  else
+    p "10の倍数との差は#{diff}です"
   end
-
-  sum = 0
-  unique_array.each do |num|
-    sum += num
-  end
-  p sum
 end
 
-lone_sum([1, 2, 3]) 
-lone_sum([3, 2, 3]) 
-lone_sum([3, 3, 3]) 
+near_ten(117)
+near_ten(123)
+near_ten(111)
