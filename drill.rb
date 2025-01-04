42f45b4c2792864f3401de0802f61bafcd26a26f
kimu805 <kimukimuok805@gmail.com>
2025-01-04 11:37:45 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,48 +1,39 @@
 def post_item(a_cart)
-  # 商品名・値段・個数の入力を促し、入力された値をハッシュオブジェクトで管理する
-    puts "商品名を入力してください："
-    name = gets.chomp
+  # 変数の定義
+  post = {}
+  puts "商品名を入力してください："
+  post[:name] = gets.chomp
+  puts "値段を入力してください："
+  post[:price] = gets.to_i
+  puts "個数を入力してください："
+  post[:count] = gets.to_i
+  line = "---------------------------"
 
-    puts "値段を入力してください："
-    price = gets.to_i
+  # レビューの描画
+  puts "商品名 : #{post[:name]}\n#{line}"
+  puts "値段 : #{post[:price]}\n#{line}"
+  puts "個数 : #{post[:count]}\n#{line}"
+  puts "合計金額 :#{post[:price] * post[:count]}\n#{line}"
 
-    puts "個数を入力してください："
-    quantity = gets.to_i
-  
-    line = "---------------------------"
-    puts line
-  
-  # 入力された値（ハッシュオブジェクトで管理している値）と合計金額を表示する
-    puts "商品名 : #{name}"
-    puts "値段 : #{price}"
-    puts "個数 : #{quantity}"
-    puts "合計金額 : #{ price * quantity }"
-    puts " "
-    
-    # ハッシュを配列オブジェクトに追加する
-    item = { name: name, price: price, quantity: quantity }
-    a_cart << item
-    # a_cartをメソッドの呼び出し元に返す
-    return a_cart
+  # 配列オブジェクトに追加
+  a_cart << post
+
+  # a_cartをメソッドの呼び出し元に返す
+  return a_cart
 end
-  
+
 def check_items(a_cart)
-# 保存された全ての商品情報（商品名・値段・個数）を、商品ごとに表示する
-  a_cart.each_with_index do |item, index|
-    puts "商品#{ index + 1 }： #{ item[:name] }"
-    puts "値段：#{ item[:price] }"
-    puts "個数：#{ item[:quantity] }"
-    puts " "
+  # リストの表示
+  total_price = 0
+  line = "---------------------------"
+  a_cart.each do |post|
+    puts "#{post[:name]}/#{post[:price]}/#{post[:count]}\n#{line}"
+    total_price += post[:price] * post[:count]
   end
-
-# 全ての商品の合計金額を表示する
-  total_price = a_cart.map { |item| item[:price] * item[:quantity] }
-  puts "合計金額 : #{ total_price }"
-
+  puts "合計金額 : #{total_price}"
 end
 
 def end_program
-  # プログラムを終了させる
   exit
 end
 
@@ -50,25 +41,26 @@ def exception
   puts "入力された値は無効な値です"
 end
 
-cart = []             # 配列オブジェクトcartの生成
+cart = []         # 配列オブジェクトcartの生成
 
 while true do
-# メニューの表示
+  # メニューの表示
   puts "商品数: #{cart.length}"
   puts "[0]商品をカートに入れる"
-  unless cart.empty?       #カートに商品がない場合、[1]は選択不可
+  unless cart.empty?
     puts "[1]カートを確認する"
   end
   puts "[2]アプリを終了する"
+
   input = gets.to_i
 
   if input == 0 then
-    cart = post_item(cart)  # post_itemメソッドを呼び出す記述
+    cart = post_item(cart)  # post_itemメソッドの呼び出し
   elsif input == 1 then
-    check_items(cart) # check_itemsメソッドを呼び出す記述
+    check_items(cart) # check_itemsメソッドの呼び出し
   elsif input == 2 then
-    end_program     # end_programメソッドを呼び出す記述
+    end_program    # end_programメソッドの呼び出し
   else
-    exception           # exceptionメソッドを呼び出す記述
+    exception        # exceptionメソッドの呼び出し
   end
 end
