46555a9dde1ecc7c0a2a889f5ea391fb4f610c49
kimu805 <kimukimuok805@gmail.com>
2025-01-05 19:16:46 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,66 +1,20 @@
-def register_book(books)
-  line = "--------------------------------"
-  print "著者を入力してください： "
-  author = gets.chomp
+require "date"
 
-  print "タイトルを入力してください： "
-  title = gets.chomp
-
-  print "価格を入力してください： "
-  price = gets.to_i
-
-  book = { author: author, title: title, price: price }
-  books << book
-  puts "「#{ title }」の登録が完了しました"
-  puts line
-end
-
-def show_books(books)
-  line = "--------------------------------"
-  puts line2
-
-  total_prices = books.map { |book| book[:price] }.sum
-  average_price = total_prices / books.size
-  puts "平均価格： #{ average_price }円"
-  
-  puts "見たい番号を入力してください"
-  books.each_with_index do |book, index|
-    puts "#{ index + 1 } : #{ book[:title] }"
+def get_days(year, month)
+  if month < 1 || month > 12
+    return "無効な値です"
   end
-  puts line
 
-  input = gets.to_i
-  show_book(books[input - 1])
+  days_in_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  days_in_months[1] = 29 if Date.leap?(year)
+
+  return days_in_months[month - 1]
 end
 
-def show_book(book)
-  line = "--------------------------------"
-  puts line
+puts "年を入力してください："
+year = gets.to_i
+puts "月を入力してください："
+month = gets.to_i
 
-  puts "著者： #{ book[:author] }"
-  puts "タイトル： #{ book[:title] }"
-  puts "価格： #{ book[:price] }"
-
-  puts line
-end
-
-books = []
-
-while true do
-  puts "番号を入力してください"
-  puts "[1] 本を登録する"
-  puts "[2] 本の一覧を見る"
-  puts "[3] 終了する"
-
-  input = gets.to_i
-  case input
-  when 1
-    register_book(books)
-  when 2
-    show_books(books)
-  when 3
-    exit
-  else
-    puts "無効な値です"
-  end
-end
+days = get_days(year, month)
+puts "#{year}年#{month}月は#{days}日間あります"
