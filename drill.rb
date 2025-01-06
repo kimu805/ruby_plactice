ec6b06ae5ea46d3de7318df16c9e6bc2bb31cb85
kimu805 <kimukimuok805@gmail.com>
2025-01-06 18:56:17 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,26 +1,26 @@
 def janken
-    hands = %W[グー チョキ パー]
-    hands.each_with_index do |hand, index|
-      puts "[#{ index }] : #{ hand }"
-    end
-    
-    player_hand = gets.to_i
-    program_hand = rand(0..2)
+  hands = %W[グー チョキ パー]
+  hands.each_with_index do |hand, index|
+    puts "[#{ index }] : #{ hand }"
+  end
+  
+  player_hand = gets.to_i
+  program_hand = rand(0..2)
 
-    puts "あなたの手：#{ hands[player_hand] }, 私の手：#{ hands[program_hand] }"
-    
-    result = (player_hand - program_hand) % 3
-    case result
-    when 0
-      puts "あいこで..."
-      return true
-    when 1
-      puts "あなたの負けです。"
-      return false
-    when 2
-      puts "あなたの勝ちです。"
-      return false
-    end
+  puts "あなたの手：#{ hands[player_hand] }, 私の手：#{ hands[program_hand] }"
+  
+  result = (player_hand - program_hand) % 3
+  case result
+  when 0
+    puts "あいこで..."
+    return true
+  when 1
+    puts "あなたの負けです。"
+    return false
+  when 2
+    puts "あなたの勝ちです。"
+    return false
+  end
 end
 
 next_game = true
