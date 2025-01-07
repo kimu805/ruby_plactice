b0b2fdba689062b5ffdefd794a3c402084176264
kimu805 <kimukimuok805@gmail.com>
2025-01-07 18:19:41 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,3 +1,16 @@
 def registration_student(students)
   
 end
+
+def show_students(students)
+  
+end
+
+students = []
+
+while true do
+  puts "行いたい項目を選択してください。"
+  puts "[1]点数を登録する"
+  puts "[2]点数を確認する"
+  puts "[3]終了する"
+end
