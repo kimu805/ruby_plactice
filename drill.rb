589b18d8b9a8c71d13f3a15c730bb3926ffb1f3d
kimu805 <kimukimuok805@gmail.com>
2025-01-09 11:05:43 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,8 +1,26 @@
-def lone_sum(array)
-  unique_numbers = array.select { |number| array.count(number) == 1 }
-  puts unique_numbers.sum
+def registration_student(students)
+  
 end
 
-lone_sum([1, 2, 3])
-lone_sum([3, 2, 3])
-lone_sum([3, 3, 3])
+def show_students(students)
+  
+end
+
+students = []
+
+while true
+  puts '行いたい項目を選択してください'
+  puts '[1]点数を登録する'
+  puts '[2]点数を確認する'
+  puts '[3]終了する'
+  input = gets.to_i
+  if input == 1
+    registration_student
+  elsif input == 2
+    show_student_name
+  elsif input == 3
+    # アプリケーションを終了させなさい
+  else
+    puts '無効な値です'
+  end
+end
