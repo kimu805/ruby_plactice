92f389a4e7e44e9ee2926ff82166aa942b7137c8
kimu805 <kimukimuok805@gmail.com>
2025-01-10 21:33:14 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -3,11 +3,20 @@
 #  "caffelatte"を１文字ずつの配列に分解するにはcharsメソッドを使います。
 
 def count_str1(str)
-  
+  count_hash = Hash.new(0)
+  str.length.times do |i|
+    target = str[i]
+    count_hash[target] += 1
+  end
+  p count_hash
 end
 
 def count_str2(str)
-  
+  count_hash = {}
+  str.chars.each do |char|
+    count_hash[char] = count_hash.fetch(char, 0) + 1
+  end
+  p count_hash
 end
 
 input = "caffelatte"
