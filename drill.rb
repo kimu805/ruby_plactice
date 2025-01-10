ea8658232a836eb8acc7bd3cbf219cdea81309e9
kimu805 <kimukimuok805@gmail.com>
2025-01-10 21:19:39 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,31 +1,3 @@
 # 文字列"caffelatte"の中で使われているアルファベットと、その回数を数えてください。
 #  ヒント：ハッシュを作って、アルファベットの各文字をキー、回数を値にセットしていきます。 
 #  "caffelatte"を１文字ずつの配列に分解するにはcharsメソッドを使います。
-
-def count_str(str)
-  # length = str.length
-
-  # hash = {}
-  # length.times do |i|
-  #   target = str[i]
-
-  #   if hash[target]
-  #     hash[target] += 1
-  #   else
-  #     hash[target] = 1
-  #   end
-  #   p hash
-  # end
-
-  # p hash
-  char_count = {}
-
-  str.chars.each do |char|
-    char_count[char] = char_count.fetch(char, 0) + 1
-  end
-
-  puts char_count
-end
-
-input = "caffelattecaffelatte"
-count_str(input)
