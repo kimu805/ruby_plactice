749f729f19687ecad0d77c33bb57b790bf0d6402
kimu805 <kimukimuok805@gmail.com>
2025-01-11 20:39:32 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -3,8 +3,13 @@ def bubble_sort(array)
   size.times do
     (0...size).each do |i|
       if array[i] > array[i + 1]
-        
+        array[i], array[i+1] = array[i+1], array[i]
       end
+      p array
     end
   end
+  array
 end
+
+array = [100,80,10,30,70,20,40,50,60,90]
+p bubble_sort(array)
