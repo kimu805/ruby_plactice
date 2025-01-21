9afa936d64f39fd828c72d178b13393147638d3f
kimu805 <kimukimuok805@gmail.com>
2025-01-21 18:07:52 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,5 +1,15 @@
 def select_sort(array)
-
+  size = array.size
+  size.times do |i|
+    min_index = i
+    (i+1...size).each do |j|
+      if array[j] < array[min_index]
+        min_index = j
+      end
+    end
+    array[i], array[min_index] = array[min_index], array[i]
+  end
+  array
 end
 
 array = [5, 6, 7, 4, 1, 3, 4, 9, 14, 41, 341, 15, 55, 51, 554, 9, 154]
