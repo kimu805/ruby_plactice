40c81bdd243bebd37867e9be6aaf0b22d614338d
kimu805 <kimukimuok805@gmail.com>
2025-01-21 18:32:03 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,16 +1,21 @@
-def select_sort(array)
-  size = array.size
-  size.times do |i|
-    min_index = i
-    (i+1...size).each do |j|
-      if array[j] < array[min_index]
-        min_index = j
-      end
-    end
-    array[i], array[min_index] = array[min_index], array[i]
-  end
-  array
-end
+array = [64, 25, 12, 22, 11]
+
+# def select_sort(array)
+#   size = array.size
+#   size.times do |i|
+#     min_index = i
+#     (i+1...size).each do |j|
+#       if array[j] < array[min_index]
+#         min_index = j
+#       end
+#     end
+#     array[i], array[min_index] = array[min_index], array[i]
+#   end
+#   array
+# end
+
+# select_sorted_array = select_sort(array)
+# puts "ソート後：#{select_sorted_array}"
 
 def bubble_sort(array)
   size = array.size
@@ -19,17 +24,11 @@ def bubble_sort(array)
       if array[i] > array[i+1]
         array[i], array[i+1] = array[i+1], array[i]
       end
+      p array
     end
   end
   array
 end
-
-array = [5, 6, 7, 4, 1, 3, 4, 9, 14, 41, 341, 15, 55, 51, 554, 9, 154]
-
-select_sorted_array = select_sort(array)
-puts "ソート後：#{select_sorted_array}"
-
-p array
 
 bubble_sorted_array = bubble_sort(array)
 puts "ソート後：#{bubble_sorted_array}"
