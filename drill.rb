0f6e49e1b6bfd21ed913acfdf1e614d6bbb7b50c
kimu805 <kimukimuok805@gmail.com>
2025-01-21 18:26:33 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -12,8 +12,24 @@ def select_sort(array)
   array
 end
 
+def bubble_sort(array)
+  size = array.size
+  size.times do
+    (0...size - 1).each do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+      end
+    end
+  end
+  array
+end
+
 array = [5, 6, 7, 4, 1, 3, 4, 9, 14, 41, 341, 15, 55, 51, 554, 9, 154]
 
-sorted_array = select_sort(array)
+select_sorted_array = select_sort(array)
+puts "ソート後：#{select_sorted_array}"
 
-puts "ソート後：#{sorted_array}"
+p array
+
+bubble_sorted_array = bubble_sort(array)
+puts "ソート後：#{bubble_sorted_array}"
