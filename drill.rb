b1c914bc09ca4110b7a70602c67da6401266422f
kimu805 <kimukimuok805@gmail.com>
2025-01-22 11:59:15 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,2 +1,16 @@
 array = [64, 25, 12, 42, 11]
 
+def quick_sort(array)
+  return array if array.size <= 1
+  p array
+
+  pivot = array[0]
+
+  left = array[1..].select { |x| x <= pivot }
+  right = array[1..].select { |x| x > pivot }
+
+  quick_sort(left) + [pivot] + quick_sort(right)
+end
+
+quick_sorted_array = quick_sort(array)
+puts "クイックソート後：#{quick_sorted_array}"
