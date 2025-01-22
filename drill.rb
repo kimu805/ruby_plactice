955189b65e5cef3f31894d95d5d80f879fb15eee
kimu805 <kimukimuok805@gmail.com>
2025-01-22 15:49:06 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,18 +1,1 @@
 array = [64, 25, 12, 42, 11]
-
-def insert_sort(array)
-  (1...array.size).each do |i|
-    key = array[i]
-    j = i - 1
-
-    while j >= 0 && array[j] > key
-      array[j+1] = array[j]
-      j -= 1
-    end
-    array[j+1] = key
-  end
-  array
-end
-
-insert_sorted_array = insert_sort(array)
-puts "挿入ソート後：#{insert_sorted_array}"
