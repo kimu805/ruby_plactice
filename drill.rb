1865ebc05281cd9299f096b76de329ec0f9d0104
kimu805 <kimukimuok805@gmail.com>
2025-01-23 21:31:16 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -2,6 +2,13 @@ array = [64, 25, 12, 42, 11]
 
 def heap_sort(array)
   size = array.size
+
+  (size / 2 - 1).downto(0) do |i|
+    heapify(array, size, i)
+  end
+end
+
+def heapify(array, size, i)
   
 end
 
