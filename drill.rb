e082c87ff4089e1e56299fff31a6e8f61c8b8768
kimu805 <kimukimuok805@gmail.com>
2025-01-23 22:00:18 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -18,3 +18,21 @@ array = [64, 25, 12, 42, 11]
 # insert_sorted_array = insert_sort(array)
 # puts "挿入ソート後：#{insert_sorted_array}"
 
+# def select_sort(array)
+#   size = array.size
+
+#   (0...size).each do |i|
+#     min_index = i
+#     (i+1...size).each do |j|
+#       if array[j] < array[min_index]
+#         min_index = j
+#       end
+#     end
+#     array[i], array[min_index] = array[min_index], array[i]
+#   end
+#   array
+# end
+
+# select_sorted_array = select_sort(array)
+# puts "選択ソート後：#{select_sorted_array}"
+
