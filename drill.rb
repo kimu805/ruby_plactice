5a09fe355da9d4a5dac65a838e8e0d265804ff6c
kimu805 <kimukimuok805@gmail.com>
2025-01-23 22:13:08 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -36,28 +36,42 @@ array = [64, 25, 12, 42, 11]
 # select_sorted_array = select_sort(array)
 # puts "選択ソート後：#{select_sorted_array}"
 
-def merge_sort(array)
+# def merge_sort(array)
+#   return array if array.size <= 1
+
+#   center = array.size / 2
+#   left = merge_sort(array[0...center])
+#   right = merge_sort(array[center..-1])
+
+#   merge(left, right)
+# end
+
+# def merge(left, right)
+#   sorted = []
+#   while !left.empty? && !right.empty?
+#     if left.first <= right.first
+#       sorted << left.shift
+#     else
+#       sorted << right.shift
+#     end
+#   end
+
+#   sorted + left + right
+# end
+
+# merge_sorted_array = merge_sort(array)
+# puts "マージソート後：#{merge_sorted_array}"
+
+def quick_sort(array)
   return array if array.size <= 1
 
-  center = array.size / 2
-  left = merge_sort(array[0...center])
-  right = merge_sort(array[center..-1])
+  pivot = array[0]
+  
+  left = array[1..].select { |x| x <= pivot }
+  right = array[1..].select { |x| x > pivot }
 
-  merge(left, right)
+  quick_sort(left) + [pivot] + quick_sort(right)
 end
 
-def merge(left, right)
-  sorted = []
-  while !left.empty? && !right.empty?
-    if left.first <= right.first
-      sorted << left.shift
-    else
-      sorted << right.shift
-    end
-  end
-
-  sorted + left + right
-end
-
-merge_sorted_array = merge_sort(array)
-puts "マージソート後：#{merge_sorted_array}"
+quick_sorted_array = quick_sort(array)
+puts "クイックソート後：#{quick_sorted_array}"
