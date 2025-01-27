bfe8f6109241f8c5273a3782a73b2ff767f26c2b
kimu805 <kimukimuok805@gmail.com>
2025-01-27 18:28:48 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -3,3 +3,19 @@ random_array = [34, 7, 23, 32, 5, 62, 31, 14]
 
 # 昇順にソートされた配列（二分探索用）
 sorted_array = [3, 8, 15, 22, 27, 33, 39, 44, 50, 55]
+
+def liner_search(array, target)
+  array.each_with_index do |number, index|
+    return index + 1 if number == target
+  end
+  return -1
+end
+
+target = gets.to_i
+result = liner_search(sorted_array, target)
+
+if result == -1
+  puts "見つかりませんでした。"
+else
+  puts "#{target}は#{result}番目にあります。"
+end
