d057b967fee2a0ebaf9e778297855a491cd9b2e3
kimu805 <kimukimuok805@gmail.com>
2025-01-29 11:03:28 +0900

    Update drill.rb

---
+++ drill.rb
@@ -0,0 +1,14 @@
+num = 11
+
+while true
+  if num.to_s == num.to_s.reverse && 
+     num.to_s(8) == num.to_s(8).reverse && 
+     num.to_s(2) == num.to_s(2).reverse
+    puts num
+    break
+  end
+  num += 2
+end
+
+puts num.to_s(2)
+puts num.to_s(8)
