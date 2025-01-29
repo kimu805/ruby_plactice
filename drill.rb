e91ac9df928fd8f62c043ea594cbced10ed5e5a9
kimu805 <kimukimuok805@gmail.com>
2025-01-29 11:33:39 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,13 +1,2 @@
-num = 1000
 nums = []
 
-while num < 10000
-  str = num.to_s
-  if str[0..1].to_i * str[2..3].to_i == str.reverse.to_i
-    nums << num
-    puts num
-  end
-  num += 1
-end
-
-puts nums
