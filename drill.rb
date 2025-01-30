f073ea5805440d7ae2ab2d6ec86e8970456343fe
kimu805 <kimukimuok805@gmail.com>
2025-01-30 21:21:07 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,10 +1,25 @@
 # coins = [10, 50, 100, 500]
 # count = 0
 
-(2..15).each do |i|
-  coins.repeated_combination(i).each do |coin_set|
-    count += 1 if coin_set.inject(:+) == 1000
-    p coin_set if coin_set.inject(:+) == 1000
+# (2..15).each do |i|
+#   coins.repeated_combination(i).each do |coin_set|
+#     count += 1 if coin_set.inject(:+) == 1000
+#     p coin_set if coin_set.inject(:+) == 1000
+#   end
+# end
+
+# p count
+
+@count = 0
+
+def change(target, coins, max)
+  coin = coins.shift
+  if coins.size == 0
+    @count += 1 if target / coin <= max
+  else
+    (0..target/coin).each do |i|
+      change(target - coin * i, coins.clone, max - i)
+    end
   end
 end
 
