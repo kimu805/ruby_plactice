42f59e06f1bc7cd876ebf1a133bd96edbef73948
kimu805 <kimukimuok805@gmail.com>
2025-01-31 21:33:42 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -2,5 +2,9 @@ coins = [10, 50, 100, 500]
 count = 0
 
 (2..15).each do |i|
-
+  coins.repeated_combination(i).each do |coin_set|
+    count += 1 if coin_set.inject(:+) == 1000
+  end
 end
+
+p count
