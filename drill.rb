eeb29b166ea7cb1bb70c5359a1dfa44cf9cfc563
kimu805 <kimukimuok805@gmail.com>
2025-02-10 17:58:24 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,3 +1,12 @@
-# member?
-hash = {"banana" => "ole", "orange" => "juice"}
-p hash.member?("apple")
+battle_count = gets.to_i
+
+win_count = 0
+
+battle_count.times do
+  battle = gets.split.map(&:upcase)
+  if (battle[0] == "G" && battle[1] == "C") || (battle[0] == "C" && battle[1] == "P") || (battle[0] == "P" && battle[1] == "G")
+    win_count += 1
+  end
+end
+
+p win_count
