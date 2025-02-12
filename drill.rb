cb1db60eb0621d71b6738d53f3d819bb22067dd3
kimu805 <kimukimuok805@gmail.com>
2025-02-12 17:18:55 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,2 +1,24 @@
-str = "aaabbcccddd"
-p str.scan("c")
+def changeName(index, after_name, users)
+  index_int = index.to_i
+  target_user = users[index_int - 1]
+  target_user[:nickname] = after_name
+end
+
+input_count, change_name_count = gets.split.map(&:to_i)
+
+users = Array.new
+
+input_count.times do 
+  user = Hash.new
+  user[:nickname], user[:old], user[:birth], user[:state] = gets.split
+  users << user
+end
+
+change_name_count.times do
+  index, after_name = gets.split
+  changeName(index, after_name, users)
+end
+
+users.each do |user|
+  puts "#{user[:nickname]} #{user[:old]} #{user[:birth]} #{user[:state]}"
+end
