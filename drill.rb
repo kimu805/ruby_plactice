317ff3a65036431df5ae05bc542e1a374a5c5de1
kimu805 <kimukimuok805@gmail.com>
2025-02-12 17:54:18 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -5,6 +5,14 @@ class Employee
     @number = number
     @name = name
   end
+
+  def change_num(after_num)
+    self.number = after_num
+  end
+
+  def change_name(after_name)
+    self.name = after_name
+  end
 end
 
 input_count = gets.to_i
@@ -14,13 +22,18 @@ input_count.times do
   a, b, c = gets.split
   b_int = b.to_i
 
-  if a == "make"
+
+  case a
+  when "make"
     array << Employee.new(b_int, c)
-  elsif a == "getnum"
-    target = array[b_int - 1]
-    puts target.number
-  else
-    target = array[b_int - 1]
-    puts target.name
+  when "getnum"
+    puts array[b_int - 1].number
+  when "getname"
+    puts array[b_int - 1].name
+  when "change_num"
+    array[b_int - 1].change_num(c)
+  when "change_name"
+    array[b_int - 1].change_name(c)
   end
+
 end
