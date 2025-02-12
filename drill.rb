4ebdc81380064c2ffd5c9e771e6de8888fd8a77e
kimu805 <kimukimuok805@gmail.com>
2025-02-12 17:45:10 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,24 +1,26 @@
-def changeName(index, after_name, users)
-  index_int = index.to_i
-  target_user = users[index_int - 1]
-  target_user[:nickname] = after_name
+class Employee
+  attr_accessor :number, :name
+
+  def initialize(number, name)
+    @number = number
+    @name = name
+  end
 end
 
-input_count, change_name_count = gets.split.map(&:to_i)
+input_count = gets.to_i
 
-users = Array.new
+array = []
+input_count.times do
+  a, b, c = gets.split
+  b_int = b.to_i
 
-input_count.times do 
-  user = Hash.new
-  user[:nickname], user[:old], user[:birth], user[:state] = gets.split
-  users << user
-end
-
-change_name_count.times do
-  index, after_name = gets.split
-  changeName(index, after_name, users)
-end
-
-users.each do |user|
-  puts "#{user[:nickname]} #{user[:old]} #{user[:birth]} #{user[:state]}"
+  if a == "make"
+    array << Employee.new(b_int, c)
+  elsif a == "getnum"
+    target = array[b_int - 1]
+    puts target.number
+  else
+    target = array[b_int - 1]
+    puts target.name
+  end
 end
