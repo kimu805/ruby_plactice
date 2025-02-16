2ab5470de7692bad7de63043b473838d133d4e8e
kimu805 <kimukimuok805@gmail.com>
2025-02-16 17:28:19 +0900

    Update drill.rb

---
+++ drill.rb
@@ -0,0 +1,51 @@
+class Customer
+  attr_reader :total
+
+  def initialize
+    @total = 0
+  end
+
+  def order(item, price)
+    @total += price
+  end
+end
+
+class AdultCustomer < Customer
+  def initialize
+    super
+    @has_ordered_alcohol = false
+  end
+
+  def order(item, price)
+    if item == "alcohol"
+      @has_ordered_alcohol = true
+    elsif item == "food" && @has_ordered_alcohol
+      price -= 200
+    end
+    super(item, price)
+  end
+end
+
+class MinorCustomer < Customer
+  def order(item, price)
+    super(item, price) unless item == "alcohol"
+  end
+end
+
+number_of_people, order_count = gets.split.map(&:to_i)
+customers = []
+
+number_of_people.times do
+  age = gets.to_i
+  customers << (age >= 20 ? AdultCustomer.new : MinorCustomer.new)
+end
+
+order_count.times do
+  id, item, price = gets.split
+  id = id.to_i - 1
+  price = price.to_i
+
+  customers[id].order(item, price)
+end
+
+customers.each { |customer| p customer.total }
