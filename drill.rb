6fddb279a0097fd832754aa5c499b1e11c49f8ab
kimu805 <kimukimuok805@gmail.com>
2025-02-16 16:56:51 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,13 +1,28 @@
 class Customer
-  attr_accessor :age
+  attr_accessor :age, :price
 
   def initialize(age)
     @age = age
+    @price = 0
+  end
+
+  def food_order(price)
+    
+  end
+
+  def soft_drink_order(price)
+    
   end
 end
 
 class CustomerOver20 < Customer
   
+  def alcohol_order(price)
+    if age < 20
+      0
+    end
+
+  end
 end
 
 number_of_people, count_of_order = gets.split.map(&:to_i)
@@ -21,4 +36,13 @@ number_of_people.times do
     customer = CustomerOver20.new(age)
   end
   customers << customer
+end
+
+count_of_order.times do
+  no_customer, order_type, price = gets.split
+  no_customer = no_customer.to_i
+  price = price.to_i
+
+
+  customers[no_customer - 1].pay(price)
 end
