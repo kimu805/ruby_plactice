97a4b48ea698be1b3e473996c46b49ceb256f347
kimu805 <kimukimuok805@gmail.com>
2025-02-16 16:37:06 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,7 +1,24 @@
 class Customer
+  attr_accessor :age
 
+  def initialize(age)
+    @age = age
+  end
 end
 
-class CustomerOver20
+class CustomerOver20 < Customer
   
+end
+
+number_of_people, count_of_order = gets.split.map(&:to_i)
+customers = []
+
+number_of_people.times do
+  age = gets.to_i
+  if age < 20
+    customer = Customer.new(age)
+  else
+    customer = CustomerOver20.new(age)
+  end
+  customers << customer
 end
