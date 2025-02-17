5d20bbd0766f1a6c58570ada9034e94d3e6e9437
kimu805 <kimukimuok805@gmail.com>
2025-02-17 16:55:23 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,44 +1,69 @@
 class Hero
   attr_accessor :level, :hp, :attack, :defense, :speed, :intel, :luck
 
-  def initialize(level, hp, attack, defense, speed, intel, luck)
-    @level = level
-    @hp = hp
-    @attack = attack
-    @defense = defense
-    @speed = speed
-    @intel = intel
-    @luck = luck
+  def initialize(*args)
+    @level = args[0]
+    @hp = args[1]
+    @attack = args[2]
+    @defense = args[3]
+    @speed = args[4]
+    @intel = args[5]
+    @luck = args[6]
   end
 
-  def levelup(hp, attack, defense, speed, intel, luck)
+  def levelup(*args)
     @level += 1
-    @hp += hp
-    @attack += attack
-    @defense += defense
-    @speed += speed
-    @intel += intel
-    @luck += luck
+    @hp += args[0]
+    @attack += args[1]
+    @defense += args[2]
+    @speed += args[3]
+    @intel += args[4]
+    @luck += args[5]
   end
 
-  def muscle_training(hp, attack)
-    @hp += hp
-    @attack += attack
+  def muscle_training(*args)
+    @hp += args[0]
+    @attack += args[1]
   end
 
-  def running(defense, speed)
-    @defense += defense
-    @speed += speed
+  def running(*args)
+    @defense += args[0]
+    @speed += args[1]
   end
 
-  def study(intel)
-    @intel += intel
+  def study(*args)
+    @intel += args[0]
   end
 
-  def pray(luck)
-    @luck += luck
+  def pray(*args)
+    @luck += args[0]
   end
 end
 
 number_of_hero, event_count = gets.split.map(&:to_i)
 
+heroes = []
+
+number_of_hero.times do
+  status = gets.split.map(&:to_i)
+  heroes << Hero.new(*status)
+end
+
+event_count.times do
+  id, item, *numbers = gets.split
+  id = id.to_i - 1
+  numbers.map(&:to_i)
+
+  case item
+  when "levelup"
+    heroes[id].levelup(*numbers)
+  when "muscle_training"
+    heroes[id].muscle_training(*numbers)
+  when "running"
+    heroes[id].running(*numbers)
+  when "study"
+    heroes[id].study(*numbers)
+  when "pray"
+    heroes[id].pray(*numbers)
+  end
+end
