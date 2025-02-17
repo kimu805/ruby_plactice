47706ee94079b21c48657c32b43fbfc7a998859d
kimu805 <kimukimuok805@gmail.com>
2025-02-17 16:07:55 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -7,8 +7,13 @@ n.times do
   big_array << small_array
 end
 
-p big_array
+str = []
+str << big_array[s-1][0]
+k.times do
+  m = gets.to_i
+  current_array = big_array.select { |sub| sub.include?(str[-1]) }.flatten
+  next_array_number = current_array[m] - 1
+  str << big_array[next_array_number][0]
+end
 
-# k.times do
-  
-# end
+puts str.join
