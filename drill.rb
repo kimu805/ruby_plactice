504b1890807b8603027b55364286f55407bc4a22
kimu805 <kimukimuok805@gmail.com>
2025-02-18 11:27:21 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,3 +1,14 @@
-x, y, z = 1, [2, 3]
+target_nums = []
+(1..10000).each do |n|
+  first = n
+  begin
+    if n % 2 == 0
+      n = n / 2
+    else
+      n = n * 3 + 1
+    end
+  end while n == first || n == 1
+  target_nums << first if n == first
+end
 
-p z
+p target_nums.size
