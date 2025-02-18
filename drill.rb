0ece2e56c2d2df2d911994110bd7e330c671bc25
kimu805 <kimukimuok805@gmail.com>
2025-02-18 11:42:03 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -15,3 +15,16 @@ even_nums.each do |n|
 end
 
 p target_nums.size
+
+def is_loop(n)
+  check = n * 3 + 1
+  while check != 1
+    check = check.even? ? check / 2 : check * 3 + 1
+    return true if check == n
+  end
+  return false
+end
+
+puts 2.step(10000, 2).count { |i|
+  is_loop(i)
+}
