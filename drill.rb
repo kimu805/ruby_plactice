6e5e89f60cddcf9342c48f612dc3747547548046
kimu805 <kimukimuok805@gmail.com>
2025-02-24 18:01:01 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -5,7 +5,9 @@ board = H.times.map { gets.chomp }
 H.times do |i|
   W.times do |j|
     if (i == 0 && board[i+1][j] == "#") || (i == H-1 && board[i-1][j] == "#") || (board[i-1][j] == "#" && board[i+1][j] == "#")
-      puts "#{i} #{j}"
+      if (board[i][j+1] == "#" && board[i][j-1] == "#") || (j == 0 && board[i][j+1] == "#") || (j == W-1 && board[i][j-1] == "#")
+        puts "#{i} #{j}"
+      end
     end
   end
 end
