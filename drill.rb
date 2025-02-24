2619674deb586a8f698c3222f16925c1741a76bc
kimu805 <kimukimuok805@gmail.com>
2025-02-24 18:09:39 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,13 +1,19 @@
-H, W = gets.split.map(&:to_i)
+h, w = gets.split.map(&:to_i)
+board = h.times.map { gets.split("") }
 
-board = H.times.map { gets.chomp }
+board.each.with_index do |row, y|
+  row.each_index do |x|
+    flag_row = false
+    flag_clm = false
 
-H.times do |i|
-  W.times do |j|
-    if (i == 0 && board[i+1][j] == "#") || (i == H-1 && board[i-1][j] == "#") || (board[i-1][j] == "#" && board[i+1][j] == "#")
-      if (board[i][j+1] == "#" && board[i][j-1] == "#") || (j == 0 && board[i][j+1] == "#") || (j == W-1 && board[i][j-1] == "#")
-        puts "#{i} #{j}"
-      end
+    if x == 0 || row[x-1] == "#"
+      flag_row = true if x == w - 1 || row[x+1] == "#"
     end
+
+    if y == 0 || board[y-1][x] == "#"
+      flag_clm = true if y == h - 1 || board[y+1][x] == "#"
+    end
+
+    puts y.to_s + " " + x.to_s if flag_row && flag_clm
   end
 end
