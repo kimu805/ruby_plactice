e77a3d8bbf552d1ea89a99eca91815bc1a4705e5
kimu805 <kimukimuok805@gmail.com>
2025-02-24 16:10:39 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,16 +1,8 @@
 H, W, N = gets.split.map(&:to_i)
 
-arr = []
-
-H.times do 
-  str = gets.chomp
-  arr << str
-end
-
+board = H.times.map { gets.chomp }
 prompt = N.times.map { gets.split.map(&:to_i) }
 
-puts "----------------"
-
 prompt.each do |x, y|
-  puts arr[x][y]
+  puts board[x][y]
 end
