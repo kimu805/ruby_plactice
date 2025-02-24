7a5a8390635422da20b12a7f5ce52d4aa3efa480
kimu805 <kimukimuok805@gmail.com>
2025-02-24 16:17:24 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,12 +1,4 @@
-H, W, N = gets.split.map(&:to_i)
+H, W = gets.split.map(&:to_i)
 
 board = H.times.map { gets.chomp }
-prompt = N.times.map { gets.split.map(&:to_i) }
 
-prompt.each do |x, y|
-  board[x][y] = "#"
-end
-
-board.each do |row|
-  puts row
-end
