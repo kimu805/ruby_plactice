0bf6cfb80e22a4e52e99e74e203e976585b989ab
kimu805 <kimukimuok805@gmail.com>
2025-02-24 16:23:51 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -2,3 +2,10 @@ H, W = gets.split.map(&:to_i)
 
 board = H.times.map { gets.chomp }
 
+H.times do |i|
+  W.times do |j|
+    if (board[i][j+1] == "#" && board[i][j-1] == "#") || (j == 0 && board[i][j+1] == "#") || (j == W-1 && board[i][j-1] == "#")
+      puts "#{i}, #{j}"
+    end
+  end
+end
