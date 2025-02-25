d039ab74a3a1e85f43b87035902ec39fa8e0ae8f
kimu805 <kimukimuok805@gmail.com>
2025-02-25 11:39:55 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,4 +1,4 @@
-y, x, D = gets.split
+y, x, D = gets.split.map.with_index { |val, i| i != 2 ? val.to_i : val }
 y = y.to_i
 x = x.to_i
 
@@ -6,9 +6,9 @@ directions = { N: [-1, 1], E: [-1, 1], S: [1, -1], W: [1, -1] }
 d = gets.chomp
 
 case D
-when "N" || "S"
+when "N" , "S"
   x += d == "L" ? directions[D.to_sym][0] : directions[D.to_sym][1]
-when "E" || "W" 
+when "E" , "W" 
   y += d == "L" ? directions[D.to_sym][0] : directions[D.to_sym][1]
 end
 
