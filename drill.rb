aadaf5a70d822f94820ee0feb8bec2285d8d96ac
kimu805 <kimukimuok805@gmail.com>
2025-02-25 11:07:09 +0900

    Update drill.rb

--- drill.rb
+++
@@ -1,7 +0,0 @@
-h, w = gets.split.map(&:to_i)
-
-board = h.times.map { gets.split("") }
-
-board.each_with_index do |row, y|
-  row.each_with_index { |val, x| puts y.to_s + " " + x.to_s if val == "#"}
-end
