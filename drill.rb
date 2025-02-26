0bf752cd2a5f45b0cc5c88212a5692084916e1b9
kimu805 <kimukimuok805@gmail.com>
2025-02-26 15:08:35 +0900

    Update drill.rb

--- drill.rb
+++
@@ -1,6 +0,0 @@
-h = gets.to_i
-board = h.times.map { gets.chomp.split("") }
-
-count = board.flatten.each_with_object(Hash.new(0)) { |item, hash| hash[item] += 1 }
-most_common_element = count.max_by { |key, value| value }[0]
-puts most_common_element
