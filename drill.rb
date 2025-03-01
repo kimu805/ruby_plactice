2b9a8712beb8c2292fd7cf00dda1ce2e0997f086
kimu805 <kimukimuok805@gmail.com>
2025-03-01 17:20:51 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,10 +1,15 @@
 h, w, y, x, n = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp }
 
+directions = []
 move = []
 n.times do
   direction, distance = gets.split
   distance = distance.to_i
-  hash = { direction => distance }
-  move << hash
+  arr = [direction, distance]
+  move << arr
+end
+
+move.each do |d, num|
+
 end
