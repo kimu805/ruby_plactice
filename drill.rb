84cdfccbc6cf4eade176e29ad20dce3ab928efe1
kimu805 <kimukimuok805@gmail.com>
2025-03-01 17:21:32 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,15 +1,2 @@
 h, w, y, x, n = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp }
-
-directions = []
-move = []
-n.times do
-  direction, distance = gets.split
-  distance = distance.to_i
-  arr = [direction, distance]
-  move << arr
-end
-
-move.each do |d, num|
-
-end
