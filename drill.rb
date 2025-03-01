7dea97e2b0638308140afb777c3be6ca40c4ea50
kimu805 <kimukimuok805@gmail.com>
2025-03-01 17:29:32 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,2 +1,15 @@
 h, w, y, x, n = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp }
+
+direction = %w[N E S W]
+curr_direct = 0
+
+n.times do
+  direct, distance = gets.split
+  distance = distance.to_i
+
+  if 
+  x += direct == direction[curr_direct % 4] ? -(distance) : distance
+
+
+end
