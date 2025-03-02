81feef39d40be99687e495a6ba4cc089b5c19f36
kimu805 <kimukimuok805@gmail.com>
2025-03-02 18:22:06 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,12 +1,12 @@
-h, w, sy, sx, n = gets.splits.map(&:to_i)
-board = h.times.map { gets.chomp.spilt("") }
+h, w, sy, sx, n = gets.split.map(&:to_i)
+board = h.times.map { gets.chomp.split("") }
 
-move = [[-1, 0], [0, 1], [1, 0], 0, -1] # (y, x)
+move = [[-1, 0], [0, 1], [1, 0], [0, -1]] # (y, x)
 now = 0
 
 y, x = sy, sx
 n.times do
-  direction, distance = gets.splits.map.with_index { |val, index| index == 1 ? val.to_i : val }
+  direction, distance = gets.split.map.with_index { |val, index| index == 1 ? val.to_i : val }
 
   if direction == "L"
     now -= 1
@@ -15,6 +15,7 @@ n.times do
   end
 
   stop = false
+  board[y][x] = "*"
   distance.times do
     ny = y + move[now % 4][0]
     nx = x + move[now % 4][1]
@@ -27,9 +28,11 @@ n.times do
     board[y][x] = "*"
   end
 
-  puts board
-
   if stop
     break
   end
+end
+
+board.each do |row|
+  p row
 end
