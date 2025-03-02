6ab2dd2fc2132f1966dd0076998481370e731467
kimu805 <kimukimuok805@gmail.com>
2025-03-02 20:21:12 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -7,7 +7,7 @@ total_people = n
 past_log = []
 logs.each_with_index do |log, index|
   last_log = logs[index - 1]
-  if !( (log.include?(words) || log[-1] != "z" || !log.include?(past_log)) || (last_log && last_log[-1] == log[0]) )
+  if !words.include?(log) || past_log.include?(log) || log[-1] == "z" || (last_log && last_log[-1] != log[0]) )
     people[index % total_people] = nil
     total_people -= 1
   end
