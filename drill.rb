280bb3575cd188ff1a1aa1d4b42035ec167be02f
kimu805 <kimukimuok805@gmail.com>
2025-03-03 17:29:53 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -12,4 +12,20 @@ s.each do |word|
 
   violate_rule_1_3 = !d.include?(word)
   violate_rule_2 = bef != "" && bef[-1] != word[0]
+  violate_rule_4 = word[-1] == "z"
+  if violate_rule_1_3 || violate_rule_2 || violate_rule_4
+    alive[player] == false
+    bef = ""
+  else
+    d.delete(word)
+    bef = word
+  end
+
+  player = (player + 1) % n
 end
+
+alive_count = 0
+alive.each { |one| alive_count += 1 if one }
+
+puts alive_count
+n.times { |i| puts i + 1 if alive[i]}
