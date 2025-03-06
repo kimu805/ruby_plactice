d1c1bc21a7a42a115bbdc18644f79cc9d1d86c8c
kimu805 <kimukimuok805@gmail.com>
2025-03-06 15:36:55 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,3 +1,9 @@
 h, w, sy, sx, n = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp.split("") }
 turns = n.times.map { gets.split.map.with_index { |val, index| index == 0 ? val.to_i : val } }
+
+move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+now = 0
+
+y, x = sy, sx
+t, d = turns.shift
