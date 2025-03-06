416680e10667745a831f30c903909893d982c093
kimu805 <kimukimuok805@gmail.com>
2025-03-06 15:35:27 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,1 +1,3 @@
 h, w, sy, sx, n = gets.split.map(&:to_i)
+board = h.times.map { gets.chomp.split("") }
+turns = n.times.map { gets.split.map.with_index { |val, index| index == 0 ? val.to_i : val } }
