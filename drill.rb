d24fb072fcb11fa6963797f97605ddb77911c71f
kimu805 <kimukimuok805@gmail.com>
2025-03-06 15:40:23 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -6,3 +6,13 @@ move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 now = 0
 
 y, x = sy, sx
+t, d = turns.shift
+100.times do |time|
+  if time == t
+    if d == "L"
+      now -= 1
+    else
+      now += 1
+    end
+    t, d = turns.shift if !turns.empty?
+  end
