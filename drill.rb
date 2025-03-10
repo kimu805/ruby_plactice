ffda8a8b51209beb1766a09386a42fe1fe2523e9
kimu805 <kimukimuok805@gmail.com>
2025-03-10 18:38:34 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -2,8 +2,8 @@ h, w = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp.split("") }
 
 done = false
-board.each.with_index do |row, y|
-  row.each.with_index do |val, x|
+board.each_with_index do |row, y|
+  row.each_with_index do |val, x|
     if val == "*"
       py = [y - 1, 0].max
       ny = [y + 1, h - 1].min
