6cc55f6efbaf4aa1941766e865ceec99f2c5c3ef
kimu805 <kimukimuok805@gmail.com>
2025-03-10 18:25:49 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,26 +1,3 @@
 h, w = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp }
 
-move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-
-sy, sx = 0
-h.times do |y|
-  w.times do |x|
-    if board[y][x] == "*"
-      sy, sx = y, x
-      break
-    end
-  end
-end
-
-move.each do |y, x|
-  ny = sy + y
-  nx = sx + x
-  if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1
-    board[ny][nx] = "*" 
-  end
-end
-
-board.each do |row|
-  puts row
-end
