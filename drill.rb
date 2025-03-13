1037ce3df6e2d996fcfd6c37d1910cf42119c47d
kimu805 <kimukimuok805@gmail.com>
2025-03-13 11:58:27 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,5 +1,15 @@
 def insert_sort(array)
-  
+  (1...array.size).each do |i|
+    key = array[i]
+    j = i - 1
+
+    while j >= 0 && array[j] > key
+      array[j+1] = array[j]
+      j -= 1
+    end
+    array[j+1] = key
+  end
+  array
 end
 
 array = [64, 25, 12, 48, 11]
