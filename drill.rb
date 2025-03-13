66de824f9a8f54047476c8962f7d90e1d89765e4
kimu805 <kimukimuok805@gmail.com>
2025-03-13 11:35:35 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,24 +1,7 @@
-h, w = gets.split.map(&:to_i)
-board = h.times.map{ gets.chomp.split("") }
-
-player = [[0, 0]]
-board.each_with_index do |row, y|
-  row.each_with_index do |val, x|
-    player[0] = [y, x] if val == "*"
-  end
+def insert_sort(array)
+  
 end
 
-move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-while !player.empty?
-  y, x = player.shift
-  board[y][x] = "*"
-  move.each do |t, s|
-    ny = y + t
-    nx = x + s
-    if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] != '#' && board[ny][nx] != '*'
-      player.push [ny, nx]
-    end
-  end
-end
-
-board.each { |row| puts row.join("") }
+array = [64, 25, 12, 48, 11]
+sorted_array = insert_sort(array)
+puts "挿入ソート後：#{sorted_array.join(" ")}"
