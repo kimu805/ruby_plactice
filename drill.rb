bd390d3373fe8937c8454098f6e19c42448d0011
kimu805 <kimukimuok805@gmail.com>
2025-03-15 17:39:02 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,23 +1,40 @@
 h, w = gets.split.map(&:to_i)
 board = h.times.map { gets.chomp.split("") }
 
-player = [[0, 0, 0]]
-board.each_with_index do |row, y|
-  row.each_with_index { |val, x| player[0] = [x, y, 0] if val == "*" }
-end
-
 move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-while !player.empty?
-  y, x, time = player.shift
-  board[y][x] = time.to_s
 
-  move.each do |t, s|
-    ny = y + t
-    nx = x + s
-    if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] == '.'
-      player.push [ny, nx, time + 1]
+player = [[0, 0]]
+stop = false
+board.each_with_index do |row, y|
+  row.each_with_index do |val, x|
+    if val == "*"
+      player[0] = [y, x]
+      board[y][x] = 0
+      stop = true
+      break
     end
   end
+  break if stop
 end
 
-board.each { |row| puts row.join("") }
+i = 1
+while !player.empty?
+  size = player.size
+  size.times do
+    target = player.shift
+    move.each do |y, x|
+      ny = target[0] + y
+      nx = target[1] + x
+      if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] == "."
+        player << [ny, nx]
+        board[ny][nx] = i
+        # binding.irb
+      end
+    end
+  end
+  i += 1
+end
+
+board.each do |row|
+  puts row.join("")
+end
