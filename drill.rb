feb17760fde866ea12f9f9b728190e36faa80b5f
kimu805 <kimukimuok805@gmail.com>
2025-03-15 17:16:05 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -4,20 +4,33 @@ board = h.times.map { gets.chomp.split("") }
 move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
 player = [[0, 0]]
-board.each_with_index { |row, y| row.each_with_index {|val, x| player[0] = [y, x] if val == "*" } }
+board.each_with_index do |row, y|
+  row.each_with_index do |val, x|
+    if val == "*"
+      player[0] = [y, x]
+      board[y][x] = 0
+      break
+    end
+  end
+  break
+end
 
+i = 1
 while !player.empty?
-  player.size.times do |i|
+  size = player.size
+  size.times do
     target = player.shift
     move.each do |y, x|
       ny = target[0] + y
       nx = target[1] + x
-      if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] != "#"
+      if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] == "."
         player << [ny, nx]
-        board[ny, nx] = i + 1
+        board[ny][nx] = i
+        # binding.irb
       end
     end
   end
+  i += 1
 end
 
 board.each do |row|
