5c7340a0d8db0fb2396a4e66e1fa3454001ab8da
kimu805 <kimukimuok805@gmail.com>
2025-03-17 11:33:03 +0900

    Update drill.rb

---
+++ drill.rb
@@ -0,0 +1,8 @@
+h, w = gets.split.map(&:to_i)
+board = h.times.map { gets.chomp.split("") }
+
+player = [[0, 0, 0]]
+board.each_with_index do |row, y|
+  row.each_with_index { |val, x| player[0] = [y, x, 0] if val == "*" }
+end
+
