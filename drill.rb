1df2deda8b07c09366618d729bc3841af7f95aef
kimu805 <kimukimuok805@gmail.com>
2025-03-18 13:29:50 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -12,3 +12,20 @@ def insert_sort(array)
   end
   array
 end
+
+def binary_search(array, target)
+  left = 0
+  right = array.size - 1
+
+  while left <= right
+    center = (left + right) / 2
+    if array[center] == target
+      return center + 1
+    elsif array[center] < target
+      left = center + 1
+    else
+      right = center - 1
+    end
+  end
+  return false
+end
