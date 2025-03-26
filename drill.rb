cc4eb598b757c256daea90804aafa6ba507b4599
kimu805 <kimukimuok805@gmail.com>
2025-03-26 14:55:33 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -20,6 +20,28 @@ turn = names.index(n.to_sym)
 while !players[:A].empty? || players[:B].empty?
   name = names[turn % 2]
   players[name].length.times do 
-    
+    y, x = players[name].shift
+
+    move.each do |t, s|
+      ny = y + t
+      nx = x + s
+      if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] == "."
+        players[name] << [ny, nx]
+        board[ny][nx] = name
+      end
+    end
+  end
+
+  turn += 1
+end
+
+teri = { A: 0, B: 0 }
+board.each do |row|
+  row.each do |val|
+    next if val == "#" || val == "."
+    teri[val] += 1
   end
 end
+
+puts teri[:A].to_s + " " + teri[:B].to_s
+puts teri.max_by { |key, val| val }[0]
