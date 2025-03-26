3d7d73aec82bfa6c9211d15a37003a877f2c7181
kimu805 <kimukimuok805@gmail.com>
2025-03-26 14:50:21 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,5 +1,25 @@
-name = "Kimura"
-age = 24
+h, w = gets.split.map(&:to_i)
+board = h.times.map { gets.chomp.split("") }
 
-user = { name:, age: }
-p user
+n = gets.chomp
+
+names = ["A".to_sym, "B".to_sym]
+players = { A: [[0,0]], b: [[0, 0]] }
+
+board.each_with_index do |row, y|
+  row.each_with_index do |val, x|
+    next if val == "#" || val == "."
+    sym = val.to_sym
+    players[sym][0] = [y, x]
+    board[y][x] = sym
+  end
+end
+
+move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+turn = names.index(n.to_sym)
+while !players[:A].empty? || players[:B].empty?
+  name = names[turn % 2]
+  players[name].length.times do 
+    
+  end
+end
