921894d48e25ef3b15982af78a870f0652a3b990
kimu805 <kimukimuok805@gmail.com>
2025-03-26 15:04:31 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,14 +1,14 @@
 h, w = gets.split.map(&:to_i)
-board = h.times.map { gets.chomp.split("") }
-
 n = gets.chomp
+board = h.times.map { gets.chomp.split('') }
 
-names = ["A".to_sym, "B".to_sym]
-players = { A: [[0,0]], b: [[0, 0]] }
+names = ['A'.to_sym, 'B'.to_sym]
+players = { A: [[0, 0]], B: [[0, 0]] }
 
-board.each_with_index do |row, y|
-  row.each_with_index do |val, x|
-    next if val == "#" || val == "."
+board.each.with_index do |row, y|
+  row.each.with_index do |val, x|
+    next if val == '#' || val == '.'
+
     sym = val.to_sym
     players[sym][0] = [y, x]
     board[y][x] = sym
@@ -17,16 +17,18 @@ end
 
 move = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 turn = names.index(n.to_sym)
-while !players[:A].empty? || players[:B].empty?
+while !players[:A].empty? || !players[:B].empty?
   name = names[turn % 2]
-  players[name].length.times do 
+  players[name].length.times do
     y, x = players[name].shift
 
     move.each do |t, s|
       ny = y + t
       nx = x + s
-      if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 && board[ny][nx] == "."
-        players[name] << [ny, nx]
+
+      if 0 <= ny && ny <= h - 1 && 0 <= nx && nx <= w - 1 &&
+           board[ny][nx] == '.'
+        players[name].push [ny, nx]
         board[ny][nx] = name
       end
     end
@@ -38,10 +40,10 @@ end
 teri = { A: 0, B: 0 }
 board.each do |row|
   row.each do |val|
-    next if val == "#" || val == "."
+    next if val == '#' || val == '.'
     teri[val] += 1
   end
 end
 
-puts teri[:A].to_s + " " + teri[:B].to_s
+puts teri[:A].to_s + ' ' + teri[:B].to_s
 puts teri.max_by { |key, val| val }[0]
