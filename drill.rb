6325dd5c4888a9cf5c14a519493d7d32f0e39e84
kimu805 <kimukimuok805@gmail.com>
2025-04-18 17:29:34 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,3 +1,16 @@
-name = "kimura"
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
 
-puts name
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send "get_#{name}_info", @id
+      price  = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info}($#{price})"
+      return "#{result}" if price >= 100
+      result
+    end
+  end
+end
