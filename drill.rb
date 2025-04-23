de3968e417286952c54f04482f26063dbe2dd3c2
kimu805 <kimukimuok805@gmail.com>
2025-04-23 18:01:34 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,16 +1,3 @@
-require "forwardable"
-class Foo 
-  extend Forwardable
-
-  def initialize
-    @buffer = []
-  end
-
-  def_delegators(:@buffer, :<<, :[])
-end
-
-f = Foo.new
-f << 1
-f << 2
-f << 3
-p f[0]
+a = 1.0 + 1
+a = a + (1/2r)
+a = a + (1 + 2i)
